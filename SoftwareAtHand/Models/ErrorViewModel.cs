7b3ae667427842349c2ef3c987695e22df1c4d4d
Sharon A. Hamon <31077791+SharonAHamon@users.noreply.github.com>
2018-12-13 12:27:03 -0700

    first commit

---
+++ SoftwareAtHand/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SoftwareAtHand.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
